87547cd37bd8f3f480f7d1bea524054f1a467fda
20200305 <69565519+20200305@users.noreply.github.com>
2020-09-13 18:39:21 +0900

    ユーザー管理機能

--- Gemfile
+++ Gemfile
@@ -54,4 +54,6 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 
- gem 'devise'
+  gem 'devise'
+
+  gem 'pry-rails'
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+          has_many :items
+          # has_many :comments
+        
+          with_options presence: true do
+            validates :nickname, format: { with: /\A[a-z0-9]+\z/i, message: 'は半角文字で入力してください' }
+            validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, massage: 'もう一度入力してください' }
+          end
 end
