55b176ce6afa6c34fa576a3270cf0db9c7315eda
20200305 <69565519+20200305@users.noreply.github.com>
2020-10-06 15:39:10 +0900

    使わない機能の削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,19 +3,11 @@ class ApplicationController < ActionController::Base
   before_action :search_item, only: [:index, :search]
 
   def index
-    @items = Item.all  # 全商品の情報を取得
+    @items = Item.all 
   end
 
-  def search
-    @results = @p.result.includes(:item) # 検索条件にマッチした商品の情報を取得
-    binding.pry
-  end
 
   private
-
-  def search_item
-    @p = Item.ransack(params[:q])  # 検索オブジェクトを生成
-  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :password, :password_confirmation])
