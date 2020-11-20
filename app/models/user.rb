d6821005862bc70506684031938b7eae8e503011
20200305 <69565519+20200305@users.noreply.github.com>
2020-11-20 20:04:16 +0900

    メールアドレ図でのログインを変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable #:validatable
 
   has_many :items
   has_many :comments
@@ -14,8 +14,8 @@ class User < ApplicationRecord
   end
 
   with_options presence: true do
-    validates :nickname
+    validates :nickname, uniqueness: true
     validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])\w{6,12}\z/i, message: 'は数字と小文字を必ず1文字使ってください' }
-    validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, massage: 'もう一度入力してください' }
+    validates :email, uniqueness: true
   end
 end
