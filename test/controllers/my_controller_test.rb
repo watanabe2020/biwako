98bbb410f5ddb86f5c3937a94aeaf8a85b3bfa8e
20200305 <69565519+20200305@users.noreply.github.com>
2020-11-28 19:07:02 +0900

    ルビコップ

--- test/controllers/my_controller_test.rb
+++ test/controllers/my_controller_test.rb
@@ -1,14 +1,13 @@
 require 'test_helper'
 
 class MyControllerTest < ActionDispatch::IntegrationTest
-  test "should get room" do
+  test 'should get room' do
     get my_room_url
     assert_response :success
   end
 
-  test "should get index" do
+  test 'should get index' do
     get my_index_url
     assert_response :success
   end
-
 end
--- test/controllers/wr_controller_test.rb
+++ test/controllers/wr_controller_test.rb
@@ -1,9 +1,8 @@
 require 'test_helper'
 
 class WrControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get wr_index_url
     assert_response :success
   end
-
 end
