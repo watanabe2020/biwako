928b38e178a9af296b2ff64a1f6e2ba3f7336bda
20200305 <69565519+20200305@users.noreply.github.com>
2020-12-21 16:20:47 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "3.14.1"
 
 set :application, "biwako"
-set :repo_url,'git@github.com:watanabe2020/biwako.git'
+set :repo_url,  'git@github.com:watanabe2020/biwako.git'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
