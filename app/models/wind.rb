f4245b9e42f1f439067a61ec509adcd20d64d63d
20200305 <69565519+20200305@users.noreply.github.com>
2021-01-06 17:35:48 +0900

    風速追加一旦

--- app/models/wind.rb
+++ app/models/wind.rb
@@ -4,6 +4,15 @@ class Wind < ActiveHash::Base
     { id: 2, name: '無風' },
     { id: 4, name: 'そよ風' },
     { id: 5, name: 'やや強い風' },
-    { id: 6, name: '釣りに影響が出るほどの強風' }
+    { id: 6, name: '釣りに影響が出るほどの強風' },
+    { id: 7, name: '1m/s以下' },
+    { id: 8, name: '1m/s' },
+    { id: 9, name: '2m/s' },
+    { id: 10, name: '3m/s' },
+    { id: 11, name: '4m/s' },
+    { id: 12, name: '5m/s' },
+    { id: 13, name: '6m/s' },
+    { id: 14, name: '7m/s' },
+    { id: 15, name: '8m/s' },
   ]
 end
