f549e4388c4f2a1c5c24bd7eebfc11bcc45d4212
20200305 <69565519+20200305@users.noreply.github.com>
2021-01-10 13:30:27 +0900

    SEO変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,10 +2,10 @@ module ApplicationHelper
   def default_meta_tags
     {
       site: 'SAISIN',
-      title: 'SAISIN 琵琶湖の最新情報！バスフィッシングの環境を知るサイト',
+      title: 'SAISIN 琵琶湖の最新情報！バスフィッシングで重要な環境を知るサイト',
       reverse: true,
       charset: 'utf-8',
-      description: '釣れた結果を知るのではなく、釣れる環境をみんなで共有するwebサイト。詳細な環境を投稿でき、共有るようなアプリケーションです。',
+      description: '釣れた結果ではなく、琵琶湖の環境をみんなで共有するwebサイト!!リアルタイムの詳細な琵琶湖の環境を投稿でき、共有しするwebサイト！アングラーみんなでバスフィッシングを盛り上げていきたい思いで制作しました。',
       keywords: '滋賀県,琵琶湖,釣り場,釣果情報,最新,琵琶湖ポイント,ポイント,SAISIN',
       canonical: request.original_url,
       separator: '|',
@@ -15,8 +15,8 @@ module ApplicationHelper
       ],
       og: {
         site_name: 'SAISIN', # もしくは site_name: :site
-        title: 'SAISIN 琵琶湖の最新情報！バスフィッシングの環境を知るサイト', # もしくは title: :title
-        description: '釣れた結果ではなく、釣れる環境をみんなで共有するwebサイト!!。', # もしくは description: :description
+        title: 'SAISIN 琵琶湖の最新情報！バスフィッシングで重要な環境を知るサイト', # もしくは title: :title
+        description: '釣れた結果ではなく、琵琶湖の環境をみんなで共有するwebサイト!!', # もしくは description: :description
         type: 'website',
         url: request.original_url,
         image: image_url('urasiro.png'),
